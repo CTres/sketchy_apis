2e942c9157b84d3ffa8123054ca6a2cfcec8f644
ctres <treseler@gmail.com>
2013-06-10 18:49:27 +0200

    new_business updated to new code style

--- dropbox_api.rb
+++ dropbox_api.rb
@@ -1,50 +1,52 @@
 require 'rubygems'
 require 'mechanize'
 
-class CreditCard 
-  attr_accessor :ccn, :expmo, :expyr, :ccode, :address, :city, :state, :zip, :country_code
-  def initialize(ccn,expmo, expyr, ccode, address, city, state, zip, country_code)
-  @ccn, @expmo, @expyr, @ccode, @address, @city, @state, @zip, @country_code = ccn, expmo, expyr, ccode, address, city, state, zip, country_code
-  end
-end
-
 class User
-  attr_accessor :fname, :lname, :email, :password, :phone, :team_num_users, :team_name, :login_email, :login_password, :name, :ccn, :expmo, :expyr, :ccode, :address, :city, :state, :zip, :country_code
-
-  def initialize(fname, lname, email, password, team_num_users, team_name, phone, login_email, login_password, name, ccn,expmo, expyr, ccode, address, city, state, zip, country_code)
-  @fname, @lname, @email, @password, @phone, @team_num_users, @team_name,
-  @name, @login_email, @login_password, @ccn, @expmo, @expyr, @ccode, @address, @city, @state, @zip, @country_code =
-
-  fname, lname, email, password, phone, team_num_users, team_name, name, login_email,
-  login_password, ccn, expmo, expyr, ccode, address, city, state, zip, country_code
+  ATTRIBUTES = [
+  :fname, :lname, :email, :password, :team_phone, :team_num_users,
+  :team_name, :login_email, :login_password, :name, :ccn, :expmo,
+  :expyr, :ccode, :address, :city, :state, :zip, :country_code
+  ]
+ 
+  attr_accessor *ATTRIBUTES
+ 
+  def initialize(attributes = {})
+    attributes.each_pair do |k, v|
+      send("#{k}=", v)
+    end
   end
-end
-
+ 
+  def to_hash
+  #create a hash of the instance variables
+    hash = {}
+    instance_variables.each do |ivar|
+      hash[ivar.to_s.delete("@")] = instance_variable_get(ivar)
+    end
+ 
+    hash
+    end
+  end
+ 
 module Dropbox
+  CONSUMER_FIELDS = %w(fname lname email password)
 
-
+  NEW_BUSINESS_FIELDS = %w(team_num_users team_name team_phone fname lname email password ccn expmo expyr ccode address city state zip country_code)
   
 
-
-
-  def self.new_consumer_account(user)
-    #create mechanize agent
-    agent = Mechanize.new{ |agent| agent.user_agent_alias = 'Mac Safari'}
+   
+  def self.consumer_account(user)
+    user = user.to_hash
+    agent = Mechanize.new{|agent| agent.user_agent_alias = 'Mac Safari' }
     agent.cookie_jar.clear!
-    page = agent.get('http://www.dropbox.com/')
+    dropbox_form = agent.get('http://www.dropbox.com/').forms[1]
+   
+    CONSUMER_FIELDS.each do |field|
+      dropbox_form.send(field.to_sym, user[field])
+    end
 
-    #pull the signup form from the dropbox page
-    dropbox_form = page.forms[1]
-
-    #set values in form fields
-    dropbox_form.fname = user.fname
-    dropbox_form.lname = user.lname
-    dropbox_form.email = user.email
-    dropbox_form.password = user.password
     dropbox_form.checkbox_with(:name => 'tos_agree').check
     page = agent.submit dropbox_form
-
-    #toss an exception if the user cannot be created.
+   
     if page.title.include? "Downloading Dropbox"
       puts "success"
     else
@@ -52,48 +54,57 @@ module Dropbox
     end
   end
 
-  def self.new_business_account(user,card)
+   
+  chuck = User.new({
+  :fname => 'charles',
+  :lname => 'treseler',
+  :email => 'colin.treseler@klarna.com',
+  :password => 'password123',
+  :team_phone => '6175640388',
+  :team_num_users => '10',
+  :team_name => 'extrala',
+  :login_email => '',
+  :login_password => '',
+  :name => 'colin treseler',
+  :ccn => '4556810589466228',
+  :ccode => '166',
+  :expmo => '12',
+  :expyr => '14',
+  :address => '79 manet rd',
+  :city => 'newton',
+  :state => 'ma',
+  :zip => '02467',
+  :country_code => 'us'
+  })
+   
+  def self.new_business_account(user)
+    
+    user = user.to_hash
     agent = Mechanize.new{ |agent| agent.user_agent_alias = 'Mac Safari'}
     agent.cookie_jar.clear!
-    page = agent.get("https://www.dropbox.com/business/try")
-    dropbox_form = page.forms[0]
-    #company details
-    dropbox_form.team_num_users = user.team_num_users
-    dropbox_form.team_name = user.team_name
-    dropbox_form.team_phone = user.phone
-
-    #create new admin account
-    dropbox_form.fname = user.fname
-    dropbox_form.lname = user.lname
-    dropbox_form.email = user.email
-    dropbox_form.password = user.password
-
-    #add billing information
-    dropbox_form.ccn = card.number
-    dropbox_form.name = card.name
-    dropbox_form.expmo = card.expmo
-    dropbox_form.expyr = card.expyr
-    dropbox_form.ccode = card.ccode
-    dropbox_form.address = card.address
-    dropbox_form.city = card.city
-    dropbox_form.state = card.state
-    dropbox_form.zip = card.zip
-    dropbox_form.country_code = user.country
-    #see if the country code works like this 
+    dropbox_form = agent.get("https://www.dropbox.com/business/try").forms[0]
+    NEW_BUSINESS_FIELDS.each do |field|
+      dropbox_form.send(field.to_sym, user[field])
+    end
+    
+    #iterator cannot handle checkbox condition, and where form field name = 'name'  
     dropbox_form.checkbox_with(:name => 'tos_agree').check
+    dropbox_form.field_with(:name => 'name').value = "#{user['fname']}" + ' ' + "#{user['lname']}"
     page = agent.submit dropbox_form
+
+    #still requiring confirmation page validation
   end
 
-  def self.convert_to_business_account(user, card)
+Dropbox.new_business_account(chuck)
+
+
+  def self.convert_to_business_account(user)
     #new business account from an existing consumer account
     agent = Mechanize.new{|agent| agent.user_agent_alias = 'Mac Safari' }
     agent.cookie_jar.clear!
-    page = agent.get("https://www.dropbox.com/business/try")
-    pp page
+    dropbox_form = agent.get("https://www.dropbox.com/business/try").forms[0]
 
-
-    dropbox_form = page.forms[0]
-    #company details
+    dropbox_form.send field.to_sym, user[field]
     dropbox_form.team_num_users = user.team_num_users
     dropbox_form.team_name = user.team_name
     dropbox_form.team_phone = user.phone
@@ -131,7 +142,7 @@ module Dropbox
     dropbox_form = page.forms[1]
     
     fields.each do |attributes|
-      dropbox_form[attributes] = user[attributes]
+      dropbox_form__send__(attributes.to_sym, user[attributes])
     end
   
   dropbox_form.checkbox_with(:name => 'tos_agree').check
@@ -140,10 +151,6 @@ module Dropbox
 
 
 end
-
-chuck = User.new('charles', 'treseler', 'colin.treseler@klarna.com', 'password123', '6175640388', '10', 'extrala', '', '', 'colin treseler', '4556810589466228', '12', '14', '166', '79 manet rd', 'newton', 'ma', '02467', 'us')
-Dropbox.consumer_account(chuck)
-#Dropbox.consumer_account(chuck)
 
 
 
